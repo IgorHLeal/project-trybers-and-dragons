cdb292b17baa99ae56e12c052d84ec525f544f95
Igor Leal <igorhenriqueleal3@gmail.com>
2022-09-22 19:20:48 -0300

    feat: Implementação da class Elf

--- src/Races/Elf.ts
+++ src/Races/Elf.ts
@@ -1,3 +1,21 @@
-export default class Elf {
-  
+import Race from './Race';
+
+export default class Elf extends Race {
+  private static _intance = 0;
+  private _maxLifePoints: number;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+
+    this._maxLifePoints = 99;
+    Elf._intance += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  public static createdRacesInstances(): number {
+    return Elf._intance;
+  }
 }
