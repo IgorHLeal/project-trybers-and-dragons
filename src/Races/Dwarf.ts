6b5598f863c241e15b073ea78406af95f1617aa7
Igor Leal <igorhenriqueleal3@gmail.com>
2022-09-22 19:19:41 -0300

    feat: Implementação da class Dwarf

--- src/Races/Dwarf.ts
+++ src/Races/Dwarf.ts
@@ -1,3 +1,21 @@
-export default class Dwarf {
-  
+import Race from './Race';
+
+export default class Dwarf extends Race {
+  private static _intance = 0;
+  private _maxLifePoints: number;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+
+    this._maxLifePoints = 80;
+    Dwarf._intance += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  public static createdRacesInstances(): number {
+    return Dwarf._intance;
+  }
 }
