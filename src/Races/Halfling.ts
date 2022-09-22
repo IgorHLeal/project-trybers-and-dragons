9223e938b7dc47492495cd47da55dcb61d09186b
Igor Leal <igorhenriqueleal3@gmail.com>
2022-09-22 19:22:10 -0300

    feat: Implementação da class Halfling

--- src/Races/Halfling.ts
+++ src/Races/Halfling.ts
@@ -1,3 +1,21 @@
-export default class Halfling {
-  
+import Race from './Race';
+
+export default class Halfling extends Race {
+  private static _intance = 0;
+  private _maxLifePoints: number;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+
+    this._maxLifePoints = 60;
+    Halfling._intance += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  public static createdRacesInstances(): number {
+    return Halfling._intance;
+  }
 }
