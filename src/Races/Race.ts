133867e0cef4d4496c310d40489dada5d4b9fb9a
Igor Leal <igorhenriqueleal3@gmail.com>
2022-09-22 18:18:05 -0300

    feat: declaração dos metodos get name e get dexterity

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -3,4 +3,12 @@ export default abstract class Race {
     private _name: string,
     private _dexterity: number,
   ) { }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get dexterity(): number {
+    return this._dexterity;
+  }
 }
