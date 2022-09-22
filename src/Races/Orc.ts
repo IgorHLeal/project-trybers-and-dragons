f1a6bb88afceb4dc0b44c4b7cbcecd695b7e63ed
Igor Leal <igorhenriqueleal3@gmail.com>
2022-09-22 19:10:07 -0300

    feat: Implementação da class Orc

---
+++ src/Races/Orc.ts
@@ -0,0 +1,21 @@
+import Race from './Race';
+
+export default class Orc extends Race {
+  private static _intance = 0;
+  private _maxLifePoints: number;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+
+    this._maxLifePoints = 74;
+    Orc._intance += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  public static createdRacesInstances(): number {
+    return Orc._intance;
+  }
+}
