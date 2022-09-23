473c21c536af6f7585b825ed95629cd33d31c61c
Igor Leal <igorhenriqueleal3@gmail.com>
2022-09-23 09:40:54 -0300

    fix: Adicionado urderscore na utilização do atributo energy dentro do método levelUp da class Character

--- src/Character.ts
+++ src/Character.ts
@@ -67,12 +67,12 @@ export default class Character implements Fighter {
     enemy.receiveDamage(this._strength);
   }
 
-  public levelUp(): void {
+  levelUp(): void {
     this._maxLifePoints += getRandomInt(1, 10);
     this._strength += getRandomInt(1, 10);
     this._dexterity += getRandomInt(1, 10);
     this._defense += getRandomInt(1, 10);
-    this.energy.amount = 10;
+    this._energy.amount = 10;
 
     if (this._maxLifePoints > this.race.maxLifePoints) {
       this._maxLifePoints = this._race.maxLifePoints;
