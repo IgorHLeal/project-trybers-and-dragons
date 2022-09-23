a3dbc94f4e428ec57f0e9a4a8fdc1a76b0c4ce2c
Igor Leal <igorhenriqueleal3@gmail.com>
2022-09-22 23:12:28 -0300

    feat: Iniciada a implementação do método levelUp estendido da interface Fighter

--- src/Character.ts
+++ src/Character.ts
@@ -5,7 +5,6 @@ import Race, { Elf } from './Races';
 import getRandomInt from './utils';
 
 export default class Character implements Fighter {
-  private _name: string;
   private _race: Race;
   private _archetype: Archetype;
   private _maxLifePoints: number;
@@ -13,13 +12,14 @@ export default class Character implements Fighter {
   private _strength: number;
   private _defense: number;
   private _dexterity: number;
-  private _energy?: Energy | undefined;
+  private _energy: Energy;
+  private _name: string;
 
   constructor(name: string) {
     this._name = name;
     this._dexterity = getRandomInt(1, 10);
-    this._race = new Elf(name, this._dexterity);
-    this._archetype = new Mage(name);
+    this._race = new Elf(this._name, this._dexterity);
+    this._archetype = new Mage(this._name);
     this._maxLifePoints = this._race.maxLifePoints / 2;
     this._lifePoints = this._maxLifePoints;
     this._strength = getRandomInt(1, 10);
@@ -30,41 +30,41 @@ export default class Character implements Fighter {
     };
   }
 
-  public get race(): Race {
+  get race(): Race {
     return this._race;
   }
 
-  public get archetype(): Archetype {
+  get archetype(): Archetype {
     return this._archetype;
   }
 
-  public get lifePoints(): number {
+  get lifePoints(): number {
     return this._lifePoints;
   }
 
-  public get strength(): number {
+  get strength(): number {
     return this._strength;
   }
 
-  public get defense(): number {
+  get defense(): number {
     return this._defense;
   }
 
-  public get dexterity(): number {
+  get dexterity(): number {
     return this._dexterity;
   }
 
-  public get energy(): Energy {
+  get energy(): Energy {
     return {
-      type_: this.energy.type_,
-      amount: this.energy.amount,
+      type_: this._energy.type_,
+      amount: this._energy.amount,
     };
   }
 
   receiveDamage(attackPoints: number): number {
     const damage = attackPoints - this._defense;
     if (damage > 0) {
-      this._lifePoints -= 1;
+      this._lifePoints -= damage;
       if (this._lifePoints <= 0) {
         this._lifePoints = -1;
       }
@@ -74,5 +74,21 @@ export default class Character implements Fighter {
 
   attack(enemy: Fighter): void {
     enemy.receiveDamage(this._strength);
+  }
+
+  levelUp(): void {
+    const newPoints = getRandomInt(1, 10);
+
+    if (this._maxLifePoints + newPoints > this.race.maxLifePoints) {
+      this._maxLifePoints = this._race.maxLifePoints;
+    } else {
+      this._maxLifePoints += newPoints;
+    }
+
+    this._lifePoints = this._maxLifePoints;
+    this._strength += getRandomInt(1, 10);
+    this._dexterity += getRandomInt(1, 10);
+    this._defense += getRandomInt(1, 10);
+    this.energy.amount = 10;
   }
 }
